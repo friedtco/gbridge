1c8111c837d556c4f5695122d3f4d0402478ac30
Christopher Friedt <chris@friedt.co>
2019-07-07 21:39:45 -0400

    Create a C++ test harness for executing
    In order to reliably run system tests between multiple processes, it's important to be able to monitor most aspects of child process such as,
    
    * exit status
    * runtime duration
    * standard input
    * standard output
    * standard error
    
    For that purpose, the "ChildProcess" class was created. An integration test was created for it as well, called "ChildProcessTest" that ensures it is able to perform the required duties.
    
    Additionally, I will now need to subclass ChildProcess, as e.g. RemoteProcess, and ensure that it is able to perform similar duties, but on a remote host (in this case Qemu) via ssh.

---
+++ .ci/test/ChildProcessTest.cpp
@@ -0,0 +1,212 @@
+#include <gtest/gtest.h>
+
+#include "ChildProcess.hpp"
+
+/*
+ * Test that demonstrates failure with an empty command
+ */
+TEST( ChildProcess, EmptyCommand ) {
+
+	int expected_int;
+	int actual_int;
+	std::string expected_string;
+	std::string actual_string;
+
+	std::vector<std::string> argv;
+	argv.push_back( "" );
+
+	ChildProcess cp( argv );
+	expected_int = ENOENT;
+	actual_int = cp.getExitStatus();
+
+	EXPECT_EQ( actual_int, expected_int );
+
+	expected_string = "";
+	actual_string = cp.getStdOut();
+	EXPECT_EQ( actual_string, expected_string );
+
+	expected_string = "";
+	actual_string = cp.getStdErr();
+	EXPECT_EQ( actual_string, expected_string );
+}
+
+/*
+ * Test that demonstrates failure with an invalid command
+ */
+TEST( ChildProcess, WhitespaceOnlyCommand ) {
+
+	int expected_int;
+	int actual_int;
+	std::string expected_string;
+	std::string actual_string;
+
+	std::vector<std::string> argv;
+	argv.push_back( " \t  \t\t " );
+
+	ChildProcess cp( argv );
+	expected_int = ENOENT;
+	actual_int = cp.getExitStatus();
+
+	EXPECT_EQ( actual_int, expected_int );
+
+	expected_string = "";
+	actual_string = cp.getStdOut();
+	EXPECT_EQ( actual_string, expected_string );
+
+	expected_string = "";
+	actual_string = cp.getStdErr();
+	EXPECT_EQ( actual_string, expected_string );
+}
+
+/*
+ * Test that demonstrates failure when the executable does not exist
+ */
+TEST( ChildProcess, NonexistentProgram ) {
+
+	int expected_int;
+	int actual_int;
+	std::string expected_string;
+	std::string actual_string;
+
+	std::vector<std::string> argv;
+	argv.push_back( "/foo/bar" );
+
+	ChildProcess cp( argv );
+	expected_int = ENOENT;
+	actual_int = cp.getExitStatus();
+
+	EXPECT_EQ( actual_int, expected_int );
+
+	expected_string = "";
+	actual_string = cp.getStdOut();
+	EXPECT_EQ( actual_string, expected_string );
+
+	expected_string = "";
+	actual_string = cp.getStdErr();
+	EXPECT_EQ( actual_string, expected_string );
+}
+
+/*
+ * This 'true' command (no stdout, just exit value of 0)
+ */
+TEST( ChildProcess, True ) {
+
+	int expected_int;
+	int actual_int;
+	std::string expected_string;
+	std::string actual_string;
+
+	std::vector<std::string> argv;
+	argv.push_back( "true" );
+
+	ChildProcess cp( argv );
+
+	expected_int = 0;
+	actual_int = cp.getExitStatus();
+	EXPECT_EQ( actual_int, expected_int );
+
+	expected_string = "";
+	actual_string = cp.getStdOut();
+	EXPECT_EQ( actual_string, expected_string );
+
+	expected_string = "";
+	actual_string = cp.getStdErr();
+	EXPECT_EQ( actual_string, expected_string );
+}
+
+/*
+ * This 'false' command (no stdout, just exit value of 1)
+ */
+TEST( ChildProcess, False ) {
+
+	int expected_int;
+	int actual_int;
+	std::string expected_string;
+	std::string actual_string;
+
+	std::vector<std::string> argv;
+	argv.push_back( "false" );
+
+	ChildProcess cp( argv );
+
+	expected_int = EXIT_FAILURE;
+	actual_int = cp.getExitStatus();
+	EXPECT_EQ( actual_int, expected_int );
+
+	expected_string = "";
+	actual_string = cp.getStdOut();
+	EXPECT_EQ( actual_string, expected_string );
+
+	expected_string = "";
+	actual_string = cp.getStdErr();
+	EXPECT_EQ( actual_string, expected_string );
+}
+
+/*
+ * This test puts some data on stdout
+ */
+TEST( ChildProcess, EchoHelloWorld ) {
+
+	int expected_int;
+	int actual_int;
+	std::string expected_string;
+	std::string actual_string;
+
+	std::string greeting( "Hello, world!" );
+
+	std::vector<std::string> argv;
+	argv.push_back( "echo" );
+	argv.push_back( greeting );
+
+	ChildProcess cp( argv );
+
+	expected_int = 0;
+	actual_int = cp.getExitStatus();
+	EXPECT_EQ( actual_int, expected_int );
+
+	expected_string = greeting + "\n";
+	actual_string = cp.getStdOut();
+	EXPECT_EQ( actual_string, expected_string );
+
+	expected_string = "";
+	actual_string = cp.getStdErr();
+	EXPECT_EQ( actual_string, expected_string );
+}
+
+/*
+ * This test ensures that a deadline of 1s is observed for executing the command
+ */
+TEST( ChildProcess, Yes ) {
+
+	int expected_int;
+	int actual_int;
+	std::string expected_string;
+	std::string actual_string;
+
+	std::vector<std::string> argv;
+	argv.push_back( "yes" );
+
+	using namespace std::chrono_literals;
+	ChildProcess cp( argv, 500ms );
+
+	// The 'yes' command is designed to run forever, and so we will definitely
+	// hit the 3s deadline and kill the program. When the process stops due to
+	// a signal, the return value is the signal that stopped it.
+	expected_int = SIGTERM;
+	actual_int = cp.getExitStatus();
+	EXPECT_EQ( actual_int, expected_int );
+
+	// count the number of y's in stdout, and make sure it's more than this
+	expected_int = 1000;
+	actual_int = 0;
+	for( auto & c: cp.getStdOut() ) {
+		if ( 'y' == c ) {
+			actual_int++;
+		}
+	}
+	EXPECT_GT( actual_int, expected_int );
+
+	expected_string = "";
+	actual_string = cp.getStdErr();
+	EXPECT_EQ( actual_string, expected_string );
+}
